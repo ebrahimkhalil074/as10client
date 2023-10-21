662ed4e185f27df1d1d588995947bae855006460
ebrahimkhalil074 <ebrahimkhalil074.com>
2023-10-21 19:14:41 +0600

    tailwind config

--- src/componentns/Navber.jsx
+++ src/componentns/Navber.jsx
@@ -27,7 +27,9 @@ const handelTheme=()=>{
    <li> <NavLink to="/login">Login</NavLink></li>
     <li><NavLink to="/register">Register</NavLink>
     </li>
-    <li onClick={handelTheme}><Link>{theme =="dark"?"light":"dark"}</Link></li>
+   {
+     <li><Link  onClick={handelTheme}>{theme =="dark"?"light":"dark"}</Link></li>
+   }
     </div>
 const handelLogOut=()=>{
   LogOut()
